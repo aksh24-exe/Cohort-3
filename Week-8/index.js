4d063aa9574d42d23a611d19f93175bfdf4e5c15
aksh24-exe <itsakshat07sharma@gmail.com>
2025-02-19 15:50:58 +0530

    Step -2 Add User Routes Skeleton

--- Week-8/index.js
+++ Week-8/index.js
@@ -2,10 +2,40 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.post("/user/signin", (req, res) => {
   res.json({
-    msg: "Hello",
+    msg: "Signin in user",
   });
 });
+
+app.post("/user/signup", (req, res) => {
+  res.json({
+    msg: "Signup in user",
+  });
+});
+
+
+app.get("/user/purchases", (req, res) => {
+  res.json({
+    msg: "Purchases Courses",
+  });
+});
+
+
+app.post("/course/purchases", (req, res) => {
+  res.json({
+    msg: "Courses Added",
+  });
+});
+
+
+app.get("/courses", (req, res) => {
+  res.json({
+    msg: "See all courses",
+  });
+});
+
 
 app.listen(3000);
