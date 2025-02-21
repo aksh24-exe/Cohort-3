2887722f282e536cf7e25daaa733fb8aac865f31
aksh24-exe <itsakshat07sharma@gmail.com>
2025-02-21 10:29:40 +0530

    Step -6 Add course route and add middleware

--- Week-8/routes/admin.js
+++ Week-8/routes/admin.js
@@ -3,6 +3,8 @@ const { adminModel } = require("../db");
 const adminRoutes = Router();
 const jwt = require("jsonwebtoken");
 const { JWT_ADMIN_PASSWORD } = require("../config");
+const { adminmiddleware } = require("../middleware/admin");
+const course = require("./course");
 
 adminRoutes.post("/signup", async (req, res) => {
   const { email, password, firstName, lastName } = req.body;
@@ -45,9 +47,56 @@ adminRoutes.post("/signin", async (req, res) => {
   }
 });
 
-adminRoutes.get("/purchases", (req, res) => {
+adminRoutes.post("/course", adminmiddleware, async (req, res) => {
+  const adminId = req.userId;
+
+  const { title, description, price, imageurl } = req.body;
+
+  const course = await adminModel.create({
+    title: title,
+    description: description,
+    price: price,
+    imageurl: imageurl,
+    courceId: adminId,
+  });
   res.json({
-    msg: "Purchases Courses",
+    msg: "Course Creates",
+    courseId: course._id,
+  });
+});
+
+adminRoutes.put("/course", adminmiddleware, async (req, res) => {
+  const adminId = req.userId;
+  const { title, description, price, imageurl, courseId } = req.body;
+
+  const course = await adminModel.updateOne(
+    {
+      _id: courseId,
+      creatorId: adminId,
+    },
+    {
+      title: title,
+      description: description,
+      price: price,
+      imageurl: imageurl,
+    }
+  );
+  res.json({
+    msg: "Course Update",
+    courseId: course._id,
+  });
+});
+
+adminRoutes.get("/course/bulk", adminmiddleware, async (req, res) => {
+  const adminId = req.userId;
+
+  const course = await adminModel.find({
+    courseId: adminId,
+  });
+
+  res.json({
+    msg: "Course Updated",
+    course,
   });
 });
 
